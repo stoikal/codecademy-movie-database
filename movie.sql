450a0240b692a34f18af4a060f3387bb2c85550c
Laksito Pamilih <laksito@pm.me>
2024-07-18 10:00:08 +0700

    finish exercise

--- movie.sql
+++ movie.sql
@@ -2,3 +2,31 @@ CREATE TABLE films (
     name TEXT,
     release_year INTEGER
 );
+
+INSERT INTO films (name, release_year)
+VALUES ('The Matrix', 1999);
+
+INSERT INTO films (name, release_year)
+VALUES ('Short Term 12', 2013);
+
+INSERT INTO films (name, release_year)
+VALUES ('In the Mood for Love', 2000);
+
+SELECT * FROM films;
+
+ALTER TABLE films
+ADD category TEXT;
+
+UPDATE films
+SET category = 'Action'
+WHERE name = 'The Matrix';
+
+UPDATE films
+SET category = 'Drama'
+WHERE name = 'Short Term 12';
+
+UPDATE films
+SET category = 'Drama'
+WHERE name = 'In the Mood for Love';
+
+SELECT * FROM films;
